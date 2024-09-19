94b1d7d63c922b5a08fcfbd7a04a44444b898ae7
Zvezdomir Dalev <zvezdomir.dalev@gmail.com>
2024-09-19 17:31:07 +0300

    added additional test if given list is less than 2

---
+++ src/task2_2nd_largest_num.py
@@ -0,0 +1,10 @@
+def get_2nd_largest(values: list[int]) -> int:
+    if len(values) < 2:
+        raise IndexError('Given list length is less than 2.')
+
+    _values = set(values)  # to remove duplicates
+
+    sort = sorted(values, reverse=True)
+    result = sort[1]
+
+    return result
--- tests/task2_2nd_largest_num/test_get_2nd_largest_number.py
+++ tests/task2_2nd_largest_num/test_get_2nd_largest_number.py
@@ -1,8 +1,11 @@
 import pytest
 
+from task2_2nd_largest_num import get_2nd_largest
 
-def get_2nd_largest(values: list[int]) -> int:
-    pass
+
+def test_when_given_parameter_is_less_than_2():
+    with pytest.raises(IndexError):
+        get_2nd_largest([1])
 
 
 def test_get_number():
